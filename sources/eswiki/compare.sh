1445481afaace5abc0b4c32656637b8275180cfa
Tony Bowden <tony@tmtm.com>
2022-07-15 14:42:28 +0300

    silence cd -

--- sources/eswiki/compare.sh
+++ sources/eswiki/compare.sh
@@ -6,4 +6,4 @@ bundle exec ruby scraper.rb | ifne tee scraped.csv
 wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid | ifne tee wikidata.csv
 bundle exec ruby diff.rb | tee diff.csv
 
-cd -
+cd ~-
